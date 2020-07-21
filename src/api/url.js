9610683cf658e228506629f7f84aafe976166de2
张锐杰 <zhangruijie@ydlkeji.com>
2020-07-21 18:35:19 +0800

    提交

--- src/api/url.js
+++ src/api/url.js
@@ -8,3 +8,22 @@ export function getUrlList(query) {
     data: query,
   });
 }
+
+
+//新增
+export function getUrlInsert(query) {
+  return request({
+    url: "/insert",
+    method: "post",
+    data: query,
+  });
+}
+
+//删除
+export function getUrlRemove(query) {
+  return request({
+    url: "/remove",
+    method: "post",
+    data: query,
+  });
+}
