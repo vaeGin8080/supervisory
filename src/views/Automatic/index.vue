fda70a5a1b084574bd513994e72699dd9ba078b6
张锐杰 <zhangruijie@ydlkeji.com>
2020-07-23 16:05:37 +0800

    监控中心动态刷新

--- src/views/Automatic/index.vue
+++ src/views/Automatic/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<ul class="list">
-		<li v-for="item in list" class="item">
+		<li v-for="item in multipleSelection" class="item">
 			<p>{{item.title}}</p>
 			<p>{{item.url}}</p>
 			<i v-if="item.status == 1" class="el-icon-success success"></i>
-			<i v-if="item.status == -1" class="el-icon-loading loading"></i>
+			<i v-if="item.status == 0" class="el-icon-loading loading"></i>
 			<i v-if="item.status == -2" class="el-icon-error error"></i>
 		</li>
 	</ul>
@@ -20,6 +20,8 @@
 			return{
 				timer: 0,
 				list:[],
+				multipleSelection:[],
+				count: 0,
 			}
 		},
 		mounted() {
@@ -28,26 +30,35 @@
 				clearInterval(this.timer)    
 			}else{      
 				this.timer = setInterval(()=>{
-					this.list = [];
 				 // 调用相应的接口，渲染数据        
 					this.init();
-				},5000)//三分钟刷新一次
+				},30000)//三分钟刷新一次
 			}  
 		},
 		methods:{
 			init(){
+				this.multipleSelection = [];
 				let that = this;
 				getUrlList().then((res) => {
 					console.log(res);
-					this.tableData = res.data.data.map((item) => {
+					res.data.data.map((item) => {
 						item.status = -1;
-						this.testing(item);
+						that.list.push(item);
 					});
+					this.count = 0;
+					this.testing(this.count);
 				});
 			},
-			testing(item){
-				this.list.push(item);
+			testing(count){
+				console.log(count);
+				let item;
+				if (typeof count == "number") {
+					item = this.list[count];
+					this.multipleSelection.push(this.list[count]);
+					console.log(this.list.length)
+				}
 				item.status = 0;
+				console.log(item);
 				let query = {
 				  id: item.id,
 				  url: item.url,
@@ -62,6 +73,9 @@
 				    } else {
 				      item.status = -2;
 				    }
+					if (count < this.list.length - 1) {
+					  this.testing(++this.count);
+					}
 				  })
 				  .catch((rej) => {
 				    item.status = -2;
@@ -82,15 +96,16 @@
 		width: 90%;
 		height: 100%;
 		margin: 0 auto;
-		display: flex;
+		/* display: flex;
 		list-style-type: none;
 		justify-content: space-between;
-		flex-wrap: wrap;
+		flex-wrap: wrap; */
 	}
 	.item{
+		float: left;
 		width: 300px;
 		height: 300px;
-		margin: 10px 0;
+		margin: 15px;
 		background: #FFFFFF;
 		border-radius: 20px;
 		border: 1px solid #13CE66;
@@ -112,11 +127,15 @@
 		color: #909399;
 	}
 	.item p{
+		width: 100%;
 		font-size: 16px;
 		font-family: PingFangSC, PingFangSC-Semibold;
 		font-weight: 600;
 		text-align: center;
 		color: #333333;
 		line-height: 30px;
+		text-overflow:ellipsis;
+		white-space:nowrap;
+		overflow:hidden;
 	}
 </style>
